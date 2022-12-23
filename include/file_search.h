9aa18cf669b71b9f25f461b5e6c66827a141ac71
andrey <andreybr02@yandex.ru>
2022-12-23 08:21:38 +0300

    get_files made

--- include/file_search.h
+++ include/file_search.h
@@ -1,4 +1,11 @@
 #pragma once
 
-#include <boost/thread.hpp>
+#include <string>
+#include <vector>
 
+#include <boost/thread.hpp>
+#include <boost/filesystem.hpp>
+
+int file_search(const boost::filesystem::path &root, const std::string target, size_t thread_nom);
+
+std::vector<boost::filesystem::path> get_files(std::string rootname);
--- source/file_search.cpp
+++ source/file_search.cpp
@@ -1,2 +1,16 @@
 #include "file_search.h"
 
+namespace bf = boost::filesystem;
+
+std::vector<bf::path> get_files(std::string rootname) {
+    bf::path root{rootname};
+    std::vector<bf::path> filenames;
+
+    for (auto const &it : bf::recursive_directory_iterator) {
+        if (bf::is_regular_file(it.path())) {
+            filenames.push_back(it.path());
+        }
+    }
+
+    return filenames;
+}
